caed2e5f00c130d7f70506fb16862b5d63f03c48
Mehran Jodavi <mehran.jodavi@gmail.com>
2020-05-08 15:59:27 -0400

    Add annotation

--- src/main/java/ca/concordia/encs/refactoring/sample/patterns/BaseDriver.java
+++ src/main/java/ca/concordia/encs/refactoring/sample/patterns/BaseDriver.java
@@ -2,8 +2,10 @@ package ca.concordia.encs.refactoring.sample.patterns;
 
 public class BaseDriver {
 
+    @Deprecated
     private void startDriver() {
         System.out.println("Start Driver");
     }
+
 
 }
