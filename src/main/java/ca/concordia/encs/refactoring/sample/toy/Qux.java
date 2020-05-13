c5d0a611811dbca264110a48aaa650cf694c3df6
Mehran Jodavi <mehran.jodavi@gmail.com>
2020-05-12 21:34:06 -0400

    Some changes that are not refactoring

--- src/main/java/ca/concordia/encs/refactoring/sample/toy/Qux.java
+++ src/main/java/ca/concordia/encs/refactoring/sample/toy/Qux.java
@@ -1,11 +1,11 @@
 package ca.concordia.encs.refactoring.sample.toy;
 
 public class Qux {
-    public void mC() {
-        System.out.println("Bar.mC()");
+    public void mE() {
+        System.out.println("Qux.mE()");
     }
 
-    public void mE() {
-        System.out.println("Bar.mE()");
+    public void mC() {
+        System.out.println("Qux.mC()");
     }
 }
