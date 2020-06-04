b401661c32c1114ef0e621a3ee1ccb8daf3fecf3
Mehran Jodavi <mehran.jodavi@gmail.com>
2020-06-04 15:31:11 -0400

    version 1 : Add Bar and Foo

---
+++ src/main/java/ca/concordia/encs/refactoring/sample/second/Bar.java
@@ -0,0 +1,19 @@
+package ca.concordia.encs.refactoring.sample.second;
+
+public class Bar {
+
+    public long mB(int a, int b) {
+        return a * b;
+    }
+
+    public double mC(int[] numbers) {
+        long sum = 0;
+        double result;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+
+        result = (double) sum / numbers.length;
+        return result;
+    }
+}
---
+++ src/main/java/ca/concordia/encs/refactoring/sample/second/Foo.java
@@ -0,0 +1,7 @@
+package ca.concordia.encs.refactoring.sample.second;
+
+public class Foo {
+    public int mA(int a, int b) {
+        return a + b;
+    }
+}
